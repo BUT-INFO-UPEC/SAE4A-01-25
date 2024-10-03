d8604f81a2fcca84c14d0e723870bcb21b5de192
mrcotin <corentin.solacoup@etu.u-pec.fr>
2024-10-03 13:36:40 +0200

    piechart

--- tests/gstatic_com.js
+++ tests/gstatic_com.js
@@ -1,8 +1,17 @@
-import "httml://www.gstatic.com/charts/loader.js"
-function createPie(dataList, legend, ) {
-    var dataTable = new google.visualization.arrayToDataTable(dataList);
+// Charger la bibliothèque Google Charts
+import "https://www.gstatic.com/charts/loader.js";
 
-    var options = {
-        title: legend
-    }
+// Fonction pour créer un graphique à partir des données et des options fournies
+function createPie(chartDiv, dataList, legend) {
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(function() {
+        var dataTable = google.visualization.arrayToDataTable(dataList);
+
+        var options = {
+            title: legend
+        };
+
+        var chart = new google.visualization.PieChart(chartDiv);
+        chart.draw(dataTable, options);
+    });
 }
