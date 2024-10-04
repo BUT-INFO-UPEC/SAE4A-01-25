a2e223de7c41459d770c5518f0e445ad29146e4a
mrcotin <corentin.solacoup@etu.u-pec.fr>
2024-10-04 10:24:12 +0200

    structure BDD table representation + structure classes dashboard et composant

--- modules_php/composant_dashboard.php
+++ modules_php/composant_dashboard.php
@@ -7,21 +7,42 @@ class Composant {
     private $attribut;
     private $aggregation;
     private $grouping;
-    private $reprType;
+    private $repr;
     private $data;
-    private $paramAffich;
+    private $params;
 
     public function __construct($composantId) {
         BDD_fetch_component($this, $composantId);
     }
 
     // tt les set et gets
+    public function setRepr($reprId) {
+        // Récupérer les détails de la représentation
+        $this->repr = BDD_fetch_visualisation($reprId);
+    }
 
     // Méthode pour générer la représentation visuelle
     public function generateVisual($data) {
-        $formatedData = "récupérer le formateur de données pour le type de données a partir de son id (reprType), surement avec un chemin dans la BDD"($data);
-        $fonctionConstructeur = "récupérer le constructeur a partir de son id (reprType), surement avec un chemin dans la BDD";
-        return $fonctionConstructeur($formatedData);
+
+        $formater = $this->repr["data_formater"];
+
+        if (function_exists($formater)) {
+            // Appeler dynamiquement la fonction
+            $formatedData = call_user_func($formater, $data);
+        } else {
+            $formatedData = $data;
+        }
+
+        // Récupérer le nom de la fonction
+        $constructor = $this->repr['visualisation_constructor'];
+        
+        if (function_exists($constructor)) {
+            // Appeler dynamiquement la fonction
+            return call_user_func($constructor, $formatedData, $this->params);
+        } else {
+            return "<p>Unsupported representation</p>";
+        }
     }
+
 }
 ?>
--- modules_php/requetteur_BDD.php
+++ modules_php/requetteur_BDD.php
@@ -1,4 +1,5 @@
 <?php
+function BDD_fetch_visualisation($reprId){}
 function BDD_fetch_component($objectToBuild, $composantId){}
 function BDD_fetch_dashboard($objectToBuild, $composantId){}
 ?>
