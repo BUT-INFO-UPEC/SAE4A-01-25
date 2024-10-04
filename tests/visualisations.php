a3912362301f1778b2423218f464430b62111470
mrcotin <corentin.solacoup@etu.u-pec.fr>
2024-10-04 11:51:56 +0200

    pb avec le diagrame circulaire

--- tests/visualisations.php
+++ tests/visualisations.php
@@ -1,19 +1,63 @@
 <?php
-$data = [
-    ['Task', 'Hours per Day'],
-    ['Work', 8],
+require_once '../modules_php/visualisations/googleCharts.php';
+
+// Données statiques pour les tests
+$pieChartData = [
+    ['Task', 'Hours'],
+    ['Work', 5],
     ['Eat', 2],
-    ['Commute', 2],
-    ['Watch TV', 4],
-    ['Sleep', 8]
+    ['Sleep', 7]
 ];
 
-// Encodage en JSON
-$data_json = json_encode($data);
+$barChartData = [
+    ['Task', 'Hours'],
+    ['Work', 5],
+    ['Exercise', 1],
+    ['Leisure', 3]
+];
 
-// Générer le HTML pour le graphique dans une variable PHP
-$chart_div = '<div id="chart_div" style="width: 900px; height: 500px;"></div>';
+$lineChartData = [
+    ['Date', 'Temperature'],
+    ['2024-01-01', 5],
+    ['2024-01-02', 7],
+    ['2024-01-03', 6],
+    ['2024-01-04', 8]
+];
 
-// Afficher le code HTML
-echo $chart_div;
+$geoChartData = [
+    ['Country', 'Value'],
+    ['France', 100],
+    ['USA', 200],
+    ['Brazil', 150]
+];
+
+// Générer les visualisations
+$pieChart = generatePieChart(1, json_encode($pieChartData), ["title" => "tst"]);
+$barChart = generateBarChart(2, json_encode($barChartData));
+$lineChart = generateLineChart(3, json_encode($lineChartData));
+$geoChart = generateGeoChart(4, json_encode($geoChartData));
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test des Représentations Météorologiques</title>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+</head>
+<body>
+    <h1>Test des Représentations Météorologiques</h1>
+    
+    <h2>Graphique Circulaire</h2>
+    <?php echo $pieChart; ?>
+    
+    <h2>Histogramme</h2>
+    <?php echo $barChart; ?>
+    
+    <h2>Courbe</h2>
+    <?php echo $lineChart; ?>
+    
+    <h2>Carte Géographique</h2>
+    <?php echo $geoChart; ?>
+</body>
+</html>
