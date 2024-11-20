0f964e71549e0ef7997f43a5e403ebb502213842
Lga92 <ahmed.92.sadoudi@gmail.com>
2024-11-20 14:46:01 +0100

    modif Header

--- layout/composants_balistiques_communs/Header.php
+++ layout/composants_balistiques_communs/Header.php
@@ -1,3 +1,12 @@
 <header>
-    
+    <h1 class="titre-header">Nom Du Site</h1>
+    <?php include "icon_account.php"; ?> 
+    <br><hr>
+    <nav class="header-menu">
+        <a href="#">jsp 1</a>
+        <a href="#">jsp 2</a>
+        <a href="#">jsp 3</a>
+        <a href="#">jsp 4</a>
+    </nav>
+    <br><hr>
 </header>
