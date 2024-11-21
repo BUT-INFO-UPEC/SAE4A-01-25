868e851499be52035bd085e08c4e4abb53246fc4
Lga92 <ahmed.92.sadoudi@gmail.com>
2024-11-21 14:59:37 +0100

    footer

--- layout/composants_balistiques_communs/Footer.php
+++ layout/composants_balistiques_communs/Footer.php
@@ -1,3 +1,11 @@
 <footer>
-    
+    <p>
+        2024 Nom du site
+        <br>
+        &copy; tout droit résérvé
+        <br>
+        API : ...
+        <br>
+        Projet académique (UPEC IUT de Créteil-Vitry - Département informatique)
+    </p>
 </footer>
