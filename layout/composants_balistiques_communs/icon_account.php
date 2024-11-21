d8ba983a865f265a0c8999d55c7b613a0d5d812a
Kirito140 <maxence.campourcy@gmail.com>
2024-11-21 14:02:08 +0100

    image dans le déroulant

--- layout/composants_balistiques_communs/icon_account.php
+++ layout/composants_balistiques_communs/icon_account.php
@@ -1,7 +1,7 @@
-<div class="dropdown position-absolute end-0">
+<div class="dropdown position-absolute end-0 bonjour">
 
     <img
-        src="<?php echo $prefixe . '../database/images/account-icon.png'; ?>"
+        src="<?php echo $prefixe . '../database/Images/account-icon.png'; ?>"
         alt="Menu"
         class="account-icon dropdown-toggle"
         id="accountDropdown"
@@ -11,21 +11,22 @@
     <ul class="dropdown-menu" aria-labelledby="accountDropdown">
         <li>
             <a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#signInModal">
-                <img src="<?php echo $prefixe . '../database/images/signin-icon.png'; ?>"
-                    alt="sign_in"
-                    class="me-2"
-                    style="width: 22px; height: 22px;">
+                <img src="<?php echo $prefixe . '../database/Images/signin-icon.png'; ?>" alt="sign_in" class="me-2">
                 Se connecter
             </a>
         </li>
         <li>
             <a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#signUpModal">
-                <img src="<?php echo $prefixe . '../database/images/signup-icon.png'; ?>"
-                    alt="sign_up"
-                    class="me-2"
-                    style="width: 22px; height: 22px;">
+                <img src="<?php echo $prefixe . '../database/Images/signup-icon.png'; ?>" alt="sign_up" class="me-2">
                 Inscription
             </a>
         </li>
     </ul>
 </div>
+
+<style>
+    .bonjour .dropdown-menu img {
+        width: 23px;
+        height: 23px;
+    }
+</style>
