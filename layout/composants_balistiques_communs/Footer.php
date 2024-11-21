4a189a86e00d83f5c65b46581e5e71f359cddd7c
Lga92 <ahmed.92.sadoudi@gmail.com>
2024-11-21 16:23:36 +0100

    j'ai oublié mdrr

--- layout/composants_balistiques_communs/Footer.php
+++ layout/composants_balistiques_communs/Footer.php
@@ -1,10 +1,11 @@
-<footer>
+<footer class="footer-layout">
     <p>
         2024 Nom du site
         <br>
         &copy; tout droit résérvé
         <br>
-        API : ...
+        API :
+        <a href="https://public.opendatasoft.com/explore/dataset/donnees-synop-essentielles-omm/information/?sort=date" targer = blank>Cliquez ici !</a>
         <br>
         Projet académique (UPEC IUT de Créteil-Vitry - Département informatique)
     </p>
