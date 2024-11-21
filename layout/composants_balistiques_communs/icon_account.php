620784913dc6b385fb63909a5f0b8ab7e7afdf3f
Kirito140 <maxence.campourcy@gmail.com>
2024-11-21 11:29:32 +0100

    ajout d'image dans le menu déroulant

--- layout/composants_balistiques_communs/icon_account.php
+++ layout/composants_balistiques_communs/icon_account.php
@@ -1,7 +1,7 @@
 <div class="dropdown position-absolute end-0">
 
     <img
-        src="<?php echo $prefixe . 'composants_balistiques_communs/account-icon.png'; ?>"
+        src="<?php echo $prefixe . 'composants_balistiques_communs/images/account-icon.png'; ?>"
         alt="Menu"
         class="account-icon dropdown-toggle"
         id="accountDropdown"
@@ -9,7 +9,26 @@
         aria-expanded="false"
         style="cursor: pointer;">
     <ul class="dropdown-menu" aria-labelledby="accountDropdown">
-        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#signInModal">Sign in</a></li>
-        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#signUpModal">Sign up</a></li>
+        <li>
+            <a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#signInModal">
+                <img src="<?php echo $prefixe . 'composants_balistiques_communs/images/signin-icon.png'; ?>"
+                    alt="Se connecter"
+                    class="me-2"
+                    style="
+                        width: 22px;
+                        height: 22px;
+                    ">
+                Se connecter
+            </a>
+        </li>
+        <li>
+            <a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#signUpModal">
+                <img src="<?php echo $prefixe . 'composants_balistiques_communs/images/signup-icon.png'; ?>"
+                    alt="Inscription"
+                    class="me-2"
+                    style="width: 16px; height: 16px;">
+                Inscription
+            </a>
+        </li>
     </ul>
 </div>
