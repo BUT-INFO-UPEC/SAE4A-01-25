fbf92a34bfa241334ba1adc87bf493fec659a17f
Solacroup Corentin <181864359+MrcotinSol@users.noreply.github.com>
2025-01-22 10:28:21 +0100

    tst

--- src/Config/ConfAPP.php
+++ src/Config/ConfAPP.php
@@ -4,11 +4,5 @@ namespace Src\Config;
 
 class ConfAPP
 {
-	public static $tCookies;
-
-	// Méthode statique pour initialiser la valeur de $tCookies
-	public static function init()
-	{
-		self::$tCookies = time() + 3600 * 24 * 7;
-	}
+	public static int $tCookies = time() + 3600 * 24 * 7;
 }
