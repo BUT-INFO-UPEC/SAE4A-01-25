72c05146d34959633618fbdfbfd8b81fbc373d88
Lga92 <ahmed.92.sadoudi@gmail.com>
2025-01-22 15:17:22 +0100

    edit footer

--- src/Views/Template/footer.php
+++ src/Views/Template/footer.php
@@ -1,7 +1,7 @@
 <footer>
-	<em>"Le climat est ce que nous attendons, le temps est ce que nous obtenons." – Mark Twain</em>
+	<em>"Face au changement climatique, nous devons faire de la nature notre alliée." – Jean Jouzel</em>
 	<br>
-    Contactez-nous : <a href="mailto:x@etu.u-pec.fr" style="color: ##007bff; text-decoration: none;">x@etu.u-pec.fr</a>
+    Contactez-nous : <a href="mailto:x@etu.u-pec.fr" style="color: ##007bff; text-decoration: none;">x@etu.u-pec.fr</a>.
 	<br>
 	Sources des données : <a href="https://public.opendatasoft.com/explore/dataset/donnees-synop-essentielles-omm/information/?sort=date"style="color: ##007bff; text-decoration: none;">API SYNOP</a>.
 	<br>
