d2b5e7d876ea68957b4a26baf44c77544e7ff687
ibolebonobo <ibrahimaydin0602@gmail.com>
2025-01-22 10:34:41 +0100

    partie php mis dans controller

--- src/Controllers/ControllerDashboard.php
+++ src/Controllers/ControllerDashboard.php
@@ -7,56 +7,90 @@ use Src\Model\Repository\Requetteur_BDD;
 
 class ControllerDashboard extends AbstractController
 {
-	static public function default(): void
-	{
-		ControllerDashboard::browse();
-	}
+    static public function default(): void
+    {
+        ControllerDashboard::browse();
+    }
 
-	static function getActionsList(): array
-	{
-		return array('Liste' => 'action=browse', 'Creation' => 'action=create');
-	}
+    static function getActionsList(): array
+    {
+        return array('Liste' => 'action=browse', 'Creation' => 'action=create');
+    }
 
-	// =======================
-	//    ENTRY METHODS
-	// =======================
-	#region entry
-	static public function browse(): void
-	{
-		$dashboards = Dashboard::get_dashboards();
+    // =======================
+    //    ENTRY METHODS
+    // =======================
+    #region entry
+    static public function browse(): void
+    {
+        $dashboards = Dashboard::get_dashboards();
 
-		$titrePage = "Liste Des Dashboards";
-		$cheminVueBody = "browse.php";
-		require('../src/Views/Template/views.php');
-	}
+        // Ajout des filtres et tri pour les tableaux de bord
+        $region = $_GET['region'] ?? null;
+        $order = $_GET['order'] ?? 'recent'; // Valeurs possibles : recent, most_viewed
+        $dateFilter = $_GET['date'] ?? 'today';
+        $customStartDate = $_GET['start_date'] ?? null;
+        $customEndDate = $_GET['end_date'] ?? null;
 
-	static function create(): void
-	{
-		$station = Requetteur_BDD::get_station();
+        // Filtrage des tableaux de bord
+        $filteredDashboards = array_filter($dashboards, function ($dash) use ($region, $dateFilter, $customStartDate, $customEndDate) {
+            $passesRegionFilter = $region ? $dash->get_region() === $region : true;
+            $passesDateFilter = true;
 
-		$titrePage = "Edition d'un Dashboard";
-		$cheminVueBody = "create.php";
-		require('../src/Views/Template/views.php');
-	}
-	#endregion entry
+            if ($dateFilter === 'yesterday') {
+                $passesDateFilter = strtotime($dash->get_date()) >= strtotime('yesterday') && strtotime($dash->get_date()) < strtotime('today');
+            } elseif ($dateFilter === 'today') {
+                $passesDateFilter = strtotime($dash->get_date()) >= strtotime('today') && strtotime($dash->get_date()) < strtotime('tomorrow');
+            } elseif ($dateFilter === 'this_week') {
+                $passesDateFilter = strtotime($dash->get_date()) >= strtotime('monday this week') && strtotime($dash->get_date()) < strtotime('monday next week');
+            } elseif ($dateFilter === 'custom' && $customStartDate && $customEndDate) {
+                $passesDateFilter = strtotime($dash->get_date()) >= strtotime($customStartDate) && strtotime($dash->get_date()) <= strtotime($customEndDate);
+            }
 
-	// =======================
-	//    GET METHODS
-	// =======================
-	#region get
-	static public function visu_dashboard(): void
-	{
-		$dash = Dashboard::get_dashboard_by_id($_GET["dashId"]);
+            return $passesRegionFilter && $passesDateFilter;
+        });
 
-		$titrePage = "Visualisatoin du Dashboard";
-		$cheminVueBody = "visu.php";
-		require('../src/Views/Template/views.php');
-	}
-	#endregion get
+        // Tri des tableaux de bord
+        usort($filteredDashboards, function ($a, $b) use ($order) {
+            if ($order === 'most_viewed') {
+                return $b->get_views() - $a->get_views();
+            } else { // Par défaut : 'recent'
+                return strtotime($b->get_date()) - strtotime($a->get_date());
+            }
+        });
 
-	// =======================
-	//    POST METHODS
-	// =======================
-	#region post
-	#endregion post
+        $titrePage = "Liste Des Dashboards";
+        $cheminVueBody = "browse.php";
+        require('../src/Views/Template/views.php');
+    }
+
+    static function create(): void
+    {
+        $station = Requetteur_BDD::get_station();
+
+        $titrePage = "Edition d'un Dashboard";
+        $cheminVueBody = "create.php";
+        require('../src/Views/Template/views.php');
+    }
+    #endregion entry
+
+    // =======================
+    //    GET METHODS
+    // =======================
+    #region get
+    static public function visu_dashboard(): void
+    {
+        $dash = Dashboard::get_dashboard_by_id($_GET["dashId"]);
+
+        $titrePage = "Visualisatoin du Dashboard";
+        $cheminVueBody = "visu.php";
+        require('../src/Views/Template/views.php');
+    }
+    #endregion get
+
+    // =======================
+    //    POST METHODS
+    // =======================
+    #region post
+    #endregion post
 }
