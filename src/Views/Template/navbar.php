26d1b8e5aa4974ea410f0b90b707772f4bb216d3
Solacroup Corentin <181864359+MrcotinSol@users.noreply.github.com>
2025-01-24 18:47:28 +0100

    changemeent de nom

--- src/Views/Template/navbar.php
+++ src/Views/Template/navbar.php
@@ -2,7 +2,7 @@
 <nav class="header-menu">
 	<a href="<?= CONTROLLER_URL ?>?controller=ControllerGeneral" class="nav-link"> Accueil </a>
 
-	<a href="<?= CONTROLLER_URL ?>?controller=ControllerDashboard&action=create" class="nav-link"> Création </a>
+	<a href="<?= CONTROLLER_URL ?>?controller=ControllerDashboard&action=new_dashboard" class="nav-link"> Création </a>
 
 	<a href="<?= CONTROLLER_URL ?>?controller=ControllerDashboard&action=browse" class="nav-link"> Liste </a>
 
