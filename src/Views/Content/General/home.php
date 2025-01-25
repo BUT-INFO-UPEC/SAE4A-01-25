9afe7af41665523a1a1ddb517c6efece3c9a9eef
Dishabht <dishabh10@gmail.com>
2025-01-26 00:00:26 +0100

    page profil correc

--- src/Views/Content/General/home.php
+++ src/Views/Content/General/home.php
@@ -1,18 +1,13 @@
-<h1> Bienvenue !! </h1>
-
-<p> Notre plateforme a été créée pour vous aider à mieux comprendre le concept de la météorologie, tout en vous offrant
-	un aperçu des conditions météorologiques actuelles en France métropolitaine et en Outre-Mer.
-
-	Explorez nos contenus pour appréhender les menaces environnementales et climatiques qui pèsent sur notre planète
-	aujourd’hui. En apprenant ensemble, nous pouvons mieux comprendre les défis du changement climatique et contribuer à
-	un avenir plus durable.
-
-	Nous vous souhaitons une agréable navigation et une expérience enrichissante sur notre site !</p>
-
-<style>
-	h1,
-	p {
-		margin-top: 20px;
-		margin-right: 30px;
-	}
-</style>
+<div class="accueil">
+	<h1> Bienvenue </h1>
+	<p> Notre plateforme a été créée pour vous aider à mieux comprendre le concept de la météorologie, tout en vous
+		offrant
+		un aperçu des conditions météorologiques actuelles en France métropolitaine et en Outre-Mer.
+		<br>
+		Explorez nos contenus pour appréhender les menaces environnementales et climatiques qui pèsent sur notre planète
+		aujourd’hui. En apprenant ensemble, nous pouvons mieux comprendre les défis du changement climatique et contribuer à
+		un avenir plus durable.
+		<br>
+		Nous vous souhaitons une agréable navigation et une expérience enrichissante sur notre site !
+	</p>
+</div>
