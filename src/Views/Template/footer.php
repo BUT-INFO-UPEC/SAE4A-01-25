10107f3c9e23683792ae35a4a740bdf73390d567
Dishabht <dishabh10@gmail.com>
2025-01-27 12:04:26 +0100

    footer modif

--- src/Views/Template/footer.php
+++ src/Views/Template/footer.php
@@ -2,37 +2,38 @@
 
 	<div class="footer-container">
 		<div class="footer-content" style="flex: 2;">
-			<h3>NOUS CONTACTER</h3>
-			<ul class="list flex gap-5">
-				<div class="">
-					<li><a href="mailto:ibrahim.aydin@etu.u-pec.fr">Ibrahim AYDIN</a></li>
-					<li><a href="mailto:disha.bhatiya@etu.u-pec.fr">Disha BHATIYA</a></li>
-					<li><a href="mailto:mohamed-amine.bouras@etu.u-pec.fr">Mohamed-Amine BOURAS</a></li>
-				</div>
-				<div class="">
-					<li><a href="mailto:maxence.campourcy@etu.u-pec.fr">Maxence CAMPOURCY</a></li>
-					<li><a href="mailto:ahmed.sadoudi@etu.u-pec.fr">Ahmed SADOUDI</a></li>
-					<li><a href="mailto:corentin.solacroup@etu.u-pec.fr">Corentin SOLACROUP</a></li>
-				</div>
+			<ul class="list">
+				<div class="footer-content">
+					<h3>NOUS CONTACTER</h3>
+					<div class="contact-list">
+						<ul>
+							<li><a href="mailto:ibrahim.aydin@etu.u-pec.fr">Ibrahim AYDIN</a></li>
+							<li><a href="mailto:disha.bhatiya@etu.u-pec.fr">Disha BHATIYA</a></li>
+							<li><a href="mailto:mohamed-amine.bouras@etu.u-pec.fr">Mohamed-Amine BOURAS</a></li>
+						</ul>
+						<ul>
+							<li><a href="mailto:maxence.campourcy@etu.u-pec.fr">Maxence CAMPOURCY</a></li>
+							<li><a href="mailto:ahmed.sadoudi@etu.u-pec.fr">Ahmed SADOUDI</a></li>
+							<li><a href="mailto:corentin.solacroup@etu.u-pec.fr">Corentin SOLACROUP</a></li>
+						</ul>
+					</div>
 			</ul>
 		</div>
+
 		<div class="footer-content">
-
-			<div class="footer-content">
-				<h3>NOUS REJOINDRE</h3>
-				<a href="https://github.com/MrcotinSol/SAE.git">GitHub</a>
-			</div>
-
-			<div class="footer-content">
-				<h3>SOURCES</h3>
-				<a href="https://public.opendatasoft.com/explore/dataset/donnees-synop-essentielles-omm/information/?sort=date">
-					API SYNOP
-			</div>
-			<div class="">
-
-				<em>"Face au changement climatique, nous devons faire de la nature notre alliée." – Jean Jouzel</em>
-				<p>© Tous droits réservés.</p>
-			</div>
+			<h3>NOUS REJOINDRE</h3>
+			<a href="https://github.com/MrcotinSol/SAE.git">GitHub</a>
 		</div>
+
+		<div class="footer-content">
+			<h3>SOURCES</h3>
+			<a href="https://public.opendatasoft.com/explore/dataset/donnees-synop-essentielles-omm/information/?sort=date">
+				API SYNOP
+		</div>
+
+	</div>
+
+	<em>"Face au changement climatique, nous devons faire de la nature notre alliée." – Jean Jouzel</em>
+	<p>© Tous droits réservés.</p>
 	</div>
 </footer>
