6f4d08bf536ad55460cdffe7e52de29316bf8be8
Solacroup Corentin <181864359+MrcotinSol@users.noreply.github.com>
2025-01-27 15:37:25 +0100

    repr

--- src/Model/DataObject/Representation.php
+++ src/Model/DataObject/Representation.php
@@ -7,17 +7,29 @@ use Src\Model\DataObject\AbstractDataObject;
 class Representation extends AbstractDataObject
 {
 	private $id;
+	private $nom;
+	private $grouppings;
 	private $fichier_visu;
 
 	function __construct($id, $nom, $grouppings, $fichier_visu)
 	{
 		$this->id = $id;
+		$this->nom = $nom;
+		$this->grouppings = $grouppings;
 		$this->fichier_visu = $fichier_visu;
 	}
 
 	function get_id()
 	{
 		return $this->id;
+	}
+	function get_nom()
+	{
+		return $this->nom;
+	}
+	function get_grouppings()
+	{
+		return $this->grouppings;
 	}
 	function get_visu_file()
 	{
