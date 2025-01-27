87a5ea3925f48981972bded0d24b690aeebbdbca
ibolebonobo <ibrahimaydin0602@gmail.com>
2025-01-27 15:50:24 +0100

    onglet

--- src/Views/Template/footer.php
+++ src/Views/Template/footer.php
@@ -4,26 +4,26 @@
 			<h3>NOUS CONTACTER</h3>
 			<div class="contact-list">
 				<ul>
-					<li><a href="mailto:ibrahim.aydin@etu.u-pec.fr" target="_blank">Ibrahim AYDIN</a></li>
-					<li><a href="mailto:disha.bhatiya@etu.u-pec.fr" target="_blank">Disha BHATIYA</a></li>
-					<li><a href="mailto:mohamed-amine.bouras@etu.u-pec.fr" target="_blank">Mohamed-Amine BOURAS</a></li>
+					<li><a href="mailto:ibrahim.aydin@etu.u-pec.fr" target="_blank" name="contact">Ibrahim AYDIN</a></li>
+					<li><a href="mailto:disha.bhatiya@etu.u-pec.fr" target="_blank" name="contact">Disha BHATIYA</a></li>
+					<li><a href="mailto:mohamed-amine.bouras@etu.u-pec.fr" target="_blank" name="contact">Mohamed-Amine BOURAS</a></li>
 				</ul>
 				<ul>
-					<li><a href="mailto:maxence.campourcy@etu.u-pec.fr" target="_blank">Maxence CAMPOURCY</a></li>
-					<li><a href="mailto:ahmed.sadoudi@etu.u-pec.fr" target="_blank">Ahmed SADOUDI</a></li>
-					<li><a href="mailto:corentin.solacroup@etu.u-pec.fr" target="_blank">Corentin SOLACROUP</a></li>
+					<li><a href="mailto:maxence.campourcy@etu.u-pec.fr" target="_blank" name="contact">Maxence CAMPOURCY</a></li>
+					<li><a href="mailto:ahmed.sadoudi@etu.u-pec.fr" target="_blank" name="contact">Ahmed SADOUDI</a></li>
+					<li><a href="mailto:corentin.solacroup@etu.u-pec.fr" target="_blank" name="contact">Corentin SOLACROUP</a></li>
 				</ul>
 			</div>
 		</div>
 
 		<div class="footer-content">
 			<h3>NOUS REJOINDRE</h3>
-			<a href="https://github.com/MrcotinSol/SAE.git" target="_blank">GitHub</a>
+			<a href="https://github.com/MrcotinSol/SAE.git" target="github">GitHub</a>
 		</div>
 
 		<div class="footer-content">
 			<h3>SOURCES</h3>
-			<a href="https://public.opendatasoft.com/explore/dataset/donnees-synop-essentielles-omm/information/?sort=date" target="_blank">
+			<a href="https://public.opendatasoft.com/explore/dataset/donnees-synop-essentielles-omm/information/?sort=date" target="synop">
 				API SYNOP
 			</a>
 		</div>
