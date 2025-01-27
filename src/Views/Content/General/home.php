0961f6121d70f37b52d0b765c524c47bf42f3262
Dishabht <dishabh10@gmail.com>
2025-01-27 16:38:26 +0100

    .

--- src/Views/Content/General/home.php
+++ src/Views/Content/General/home.php
@@ -3,14 +3,18 @@
 <div class="accueil">
 
 	<h1> BIENVENUE SUR MÉTÉOSCOPE </h1>
-	<p> Notre plateforme a été créée pour vous aider à mieux comprendre le concept de la météorologie, tout en vous
+	<p style="font-size: 16px;">
+		Notre plateforme a été créée pour vous aider à mieux comprendre le concept de la météorologie, tout en vous
 		offrant
 		un aperçu des conditions météorologiques actuelles en France métropolitaine et en Outre-Mer.
+		<br>
 		<br>
 		Explorez nos contenus pour appréhender les menaces environnementales et climatiques qui pèsent sur notre planète
 		aujourd’hui. En apprenant ensemble, nous pouvons mieux comprendre les défis du changement climatique et contribuer à
 		un avenir plus durable.
 		<br>
+		<br>
 		Nous vous souhaitons une agréable navigation et une expérience enrichissante sur notre site !
+		<br>
 	</p>
 </div>
