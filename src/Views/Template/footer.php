93431ea7095fcb14476488d0efebbff0c93c59e9
Kirito140 <maxence.campourcy@gmail.com>
2025-01-27 10:27:54 +0100

    test footer

--- src/Views/Template/footer.php
+++ src/Views/Template/footer.php
@@ -4,12 +4,12 @@
 		<div class="footer-content">
 			<h3>NOUS CONTACTER</h3>
 			<ul class="list flex gap-5">
-				<div class="flex">
+				<div class="">
 					<li><a href="mailto:ibrahim.aydin@etu.u-pec.fr">Ibrahim AYDIN</a></li>
 					<li><a href="mailto:disha.bhatiya@etu.u-pec.fr">Disha BHATIYA</a></li>
 					<li><a href="mailto:mohamed-amine.bouras@etu.u-pec.fr">Mohamed-Amine BOURAS</a></li>
 				</div>
-				<div class="flex">
+				<div class="">
 					<li><a href="mailto:maxence.campourcy@etu.u-pec.fr">Maxence CAMPOURCY</a></li>
 					<li><a href="mailto:ahmed.sadoudi@etu.u-pec.fr">Ahmed SADOUDI</a></li>
 					<li><a href="mailto:corentin.solacroup@etu.u-pec.fr">Corentin SOLACROUP</a></li>
