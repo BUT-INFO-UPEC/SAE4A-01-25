208984b4509dd5d26b4486659d7d736357a0657f
Dishabht <dishabh10@gmail.com>
2025-01-27 11:24:38 +0100

    .

--- src/Views/Template/footer.php
+++ src/Views/Template/footer.php
@@ -1,7 +1,7 @@
 <footer>
 
 	<div class="footer-container">
-		<div class="footer-content">
+		<div class="footer-content" style="flex: 2;">
 			<h3>NOUS CONTACTER</h3>
 			<ul class="list flex gap-5">
 				<div class="">
@@ -16,20 +16,23 @@
 				</div>
 			</ul>
 		</div>
-
 		<div class="footer-content">
-			<h3>NOUS REJOINDRE</h3>
-			<a href="https://github.com/MrcotinSol/SAE.git">GitHub</a>
-		</div>
 
-		<div class="footer-content">
-			<h3>SOURCES</h3>
-			<a
-				href="https://public.opendatasoft.com/explore/dataset/donnees-synop-essentielles-omm/information/?sort=date">
-				API SYNOP
+			<div class="footer-content">
+				<h3>NOUS REJOINDRE</h3>
+				<a href="https://github.com/MrcotinSol/SAE.git">GitHub</a>
+			</div>
+
+			<div class="footer-content">
+				<h3>SOURCES</h3>
+				<a href="https://public.opendatasoft.com/explore/dataset/donnees-synop-essentielles-omm/information/?sort=date">
+					API SYNOP
+			</div>
+			<div class="">
+
+				<em>"Face au changement climatique, nous devons faire de la nature notre alliée." – Jean Jouzel</em>
+				<p>© Tous droits réservés.</p>
+			</div>
 		</div>
 	</div>
-
-	<em>"Face au changement climatique, nous devons faire de la nature notre alliée." – Jean Jouzel</em>
-	<p>© Tous droits réservés.</p>
 </footer>
