7824e496f77a0055e00d92fca33b166c3f920afd
ibolebonobo <ibrahimaydin0602@gmail.com>
2025-01-27 15:48:28 +0100

    target blank

--- src/Views/Template/footer.php
+++ src/Views/Template/footer.php
@@ -4,26 +4,26 @@
 			<h3>NOUS CONTACTER</h3>
 			<div class="contact-list">
 				<ul>
-					<li><a href="mailto:ibrahim.aydin@etu.u-pec.fr">Ibrahim AYDIN</a></li>
-					<li><a href="mailto:disha.bhatiya@etu.u-pec.fr">Disha BHATIYA</a></li>
-					<li><a href="mailto:mohamed-amine.bouras@etu.u-pec.fr">Mohamed-Amine BOURAS</a></li>
+					<li><a href="mailto:ibrahim.aydin@etu.u-pec.fr" target="_blank">Ibrahim AYDIN</a></li>
+					<li><a href="mailto:disha.bhatiya@etu.u-pec.fr" target="_blank">Disha BHATIYA</a></li>
+					<li><a href="mailto:mohamed-amine.bouras@etu.u-pec.fr" target="_blank">Mohamed-Amine BOURAS</a></li>
 				</ul>
 				<ul>
-					<li><a href="mailto:maxence.campourcy@etu.u-pec.fr">Maxence CAMPOURCY</a></li>
-					<li><a href="mailto:ahmed.sadoudi@etu.u-pec.fr">Ahmed SADOUDI</a></li>
-					<li><a href="mailto:corentin.solacroup@etu.u-pec.fr">Corentin SOLACROUP</a></li>
+					<li><a href="mailto:maxence.campourcy@etu.u-pec.fr" target="_blank">Maxence CAMPOURCY</a></li>
+					<li><a href="mailto:ahmed.sadoudi@etu.u-pec.fr" target="_blank">Ahmed SADOUDI</a></li>
+					<li><a href="mailto:corentin.solacroup@etu.u-pec.fr" target="_blank">Corentin SOLACROUP</a></li>
 				</ul>
 			</div>
 		</div>
 
 		<div class="footer-content">
 			<h3>NOUS REJOINDRE</h3>
-			<a href="https://github.com/MrcotinSol/SAE.git">GitHub</a>
+			<a href="https://github.com/MrcotinSol/SAE.git" target="_blank">GitHub</a>
 		</div>
 
 		<div class="footer-content">
 			<h3>SOURCES</h3>
-			<a href="https://public.opendatasoft.com/explore/dataset/donnees-synop-essentielles-omm/information/?sort=date">
+			<a href="https://public.opendatasoft.com/explore/dataset/donnees-synop-essentielles-omm/information/?sort=date" target="_blank">
 				API SYNOP
 			</a>
 		</div>
@@ -31,5 +31,4 @@
 
 	<em>"Face au changement climatique, nous devons faire de la nature notre alliée." – Jean Jouzel</em>
 	<p>© Tous droits réservés.</p>
-
 </footer>
