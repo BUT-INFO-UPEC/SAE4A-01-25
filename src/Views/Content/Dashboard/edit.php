38c84d81aa3658206db3fc8ffe987419da362aaf
Kirito140 <maxence.campourcy@gmail.com>
2025-02-21 11:56:40 +0100

    page edit
    il y avait un "<" en trop

--- src/Views/Content/Dashboard/edit.php
+++ src/Views/Content/Dashboard/edit.php
@@ -170,7 +170,7 @@
 
 			<div class="mb-4">
 				<h4>Commentaires</h4>
-				<textarea name="comments_{{tab.id}}" id="comments_{{tab.id}}" class="form-control" rows="4" placeholder="Commentaires explicatifs de l'analyse"><?= $dash->get_comment(); ?><</textarea>
+				<textarea name="comments_{{tab.id}}" id="comments_{{tab.id}}" class="form-control" rows="4" placeholder="Commentaires explicatifs de l'analyse"><?= $dash->get_comment(); ?></textarea>
 			</div>
 		</div>
 	</div>
