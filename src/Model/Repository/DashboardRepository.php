fc08e5cd3b14ecc0061d5f4884be55f024f88e1c
Kirito140 <maxence.campourcy@gmail.com>
2025-02-21 09:52:12 +0100

    ajout des tryc dans dashrepo

--- src/Model/Repository/DashboardRepository.php
+++ src/Model/Repository/DashboardRepository.php
@@ -2,6 +2,8 @@
 
 namespace Src\Model\Repository;
 
+use Exception;
+use PDOException;
 use Src\Config\MsgRepository;
 use Src\Config\UserManagement;
 use Src\Model\DataObject\Dashboard;
@@ -25,50 +27,82 @@ class DashboardRepository extends AbstractRepository
 
 	public function arrayConstructor(array $objetFormatTableau): Dashboard
 	{
-		$composants = $this->BuildComposants($objetFormatTableau['id']);
-		$criteres_geo = $this->BuildGeo($objetFormatTableau['id']);
+		try {
+			$composants = $this->BuildComposants($objetFormatTableau['id']);
+			$criteres_geo = $this->BuildGeo($objetFormatTableau['id']);
 
-		return new Dashboard($objetFormatTableau['id'], $objetFormatTableau['privatisation'], $objetFormatTableau['createur_id'], $objetFormatTableau['date_debut'], $objetFormatTableau['date_fin'], $objetFormatTableau['date_debut_relatif'] == "True", $objetFormatTableau['date_fin_relatif'] == "True", $composants, $criteres_geo, [$objetFormatTableau['params']]);
+			return new Dashboard($objetFormatTableau['id'], $objetFormatTableau['privatisation'], $objetFormatTableau['createur_id'], $objetFormatTableau['date_debut'], $objetFormatTableau['date_fin'], $objetFormatTableau['date_debut_relatif'] == "True", $objetFormatTableau['date_fin_relatif'] == "True", $composants, $criteres_geo, [$objetFormatTableau['params']]);
+		} catch (Exception $e) {
+			MsgRepository::newError("Erreur lors de la construction du dashboard", "Le dashboard n'a pas pu être construit.\n" . $e->getMessage());
+			return null;
+		} catch (PDOException $e) {
+			MsgRepository::newError("Erreur lors de la construction du dashboard", "Le dashboard n'a pas pu être construit.\n" . $e->getMessage());
+			return null;
+		}
 	}
 
 	public function BuildGeo($id): array
 	{
-		$query = "SELECT type_critere, critere_id FROM CritereGeo_dashboard WHERE dashboard_id = :ID";
-		$values = ["ID" => $id];
-		$criteres = DatabaseConnection::fetchAll($query, $values); // récupéraiton des criteres géographiques lié a ce dashboard
+		try {
+			$query = "SELECT type_critere, critere_id FROM CritereGeo_dashboard WHERE dashboard_id = :ID";
+			$values = ["ID" => $id];
+			$criteres = DatabaseConnection::fetchAll($query, $values); // récupéraiton des criteres géographiques lié a ce dashboard
 
-		$result = [];
-		foreach ($criteres as $value) {
-			$result[DashboardRepository::TYPES_CRITERES_GEO[$value['type_critere']]][] = $value['critere_id'];
+			$result = [];
+			foreach ($criteres as $value) {
+				$result[DashboardRepository::TYPES_CRITERES_GEO[$value['type_critere']]][] = $value['critere_id'];
+			}
+
+			return $result;
+		} catch (Exception $e) {
+			MsgRepository::newError("Erreur lors de la récupération des critères géographiques", "Les critères géographiques n'ont pas pu être récupérés.\n" . $e->getMessage());
+			return [];
+		} catch (PDOException $e) {
+			MsgRepository::newError("Erreur lors de la récupération des critères géographiques", "Les critères géographiques n'ont pas pu être récupérés.\n" . $e->getMessage());
+			return [];
 		}
-
-		return $result;
 	}
 
 	public function BuildComposants($id): array
 	{
-		$query = "SELECT composant_id FROM Composant_dashboard WHERE dashboard_id = :ID";
-		$values = ["ID" => $id];
-		$composantsId = DatabaseConnection::fetchAll($query, $values); // récupéraiton des id des composants du dashboard
-		$constructeur = new ComposantRepository();
-		$composants = [];
-		foreach ($composantsId as $compId) {
-			$composants[] = $constructeur->get_composant_by_id($compId['composant_id']);
+		try {
+			$query = "SELECT composant_id FROM Composant_dashboard WHERE dashboard_id = :ID";
+			$values = ["ID" => $id];
+			$composantsId = DatabaseConnection::fetchAll($query, $values); // récupéraiton des id des composants du dashboard
+			$constructeur = new ComposantRepository();
+			$composants = [];
+			foreach ($composantsId as $compId) {
+				$composants[] = $constructeur->get_composant_by_id($compId['composant_id']);
+			}
+			return $composants;
+		} catch (Exception $e) {
+			MsgRepository::newError("Erreur lors de la récupération des composants", "Les composants n'ont pas pu être récupérés.\n" . $e->getMessage());
+			return [];
+		} catch (PDOException $e) {
+			MsgRepository::newError("Erreur lors de la récupération des composants", "Les composants n'ont pas pu être récupérés.\n" . $e->getMessage());
+			return [];
 		}
-		return $composants;
 	}
 
 	public function get_dashboard_by_id(string $id): ?Dashboard
 	{
 		// ajouter vérif appartenance a l'utilisateur ou visibilité publique
-		$values["createur_id"] = UserManagement::getUser() == null ? 0 : UserManagement::getUser()->getId();
-		$values["privatisation"] = 0;
+		try {
+			$values["createur_id"] = UserManagement::getUser() == null ? 0 : UserManagement::getUser()->getId();
+			$values["privatisation"] = 0;
 
-		$query = "and (createur_id=:createur_id or privatisation=:privatisation)";
+			$query = "and (createur_id=:createur_id or privatisation=:privatisation)";
 
-		$dash = $this->select($id, $query, $values);
+			$dash = $this->select($id, $query, $values);
 
-		return $dash;
+			return $dash;
+		} catch (Exception $e) {
+			MsgRepository::newError("Erreur lors de la récupération du dashboard", "Le dashboard n'a pas pu être récupéré.\n" . $e->getMessage());
+			return null;
+		} catch (PDOException $e) {
+			MsgRepository::newError("Erreur lors de la récupération du dashboard", "Le dashboard n'a pas pu être récupéré.\n" . $e->getMessage());
+			return null;
+		}
 	}
 
 	public function get_dashboards($criteres_geo, $order, $dateFilter, $customStartDate, $customEndDate, $privatisation): array
@@ -76,23 +110,31 @@ class DashboardRepository extends AbstractRepository
 		// construire les paramètres where et ajouter des paramètres a mettre dans une liste associative pour filtrer par visibilité entre autre
 
 		// Déterminer les paramètres (:param => valeur)
-		$values = [];
+		try {
+			$values = [];
 
-		// construire les composants de la requette "WHERE"
-		$wherequery = [];
-		$wherequery[] = $this->buildPrivatisation($values, $privatisation);
+			// construire les composants de la requette "WHERE"
+			$wherequery = [];
+			$wherequery[] = $this->buildPrivatisation($values, $privatisation);
 
-		// construire la chaine de caractère de la requette "WHERE" en assamblant ses composants
-		$wherequery = "WHERE (" . implode(") and (", $wherequery) . ")";
+			// construire la chaine de caractère de la requette "WHERE" en assamblant ses composants
+			$wherequery = "WHERE (" . implode(") and (", $wherequery) . ")";
 
-		// construire la requette "ORDER BY"
-		$orderquery = "";
+			// construire la requette "ORDER BY"
+			$orderquery = "";
 
-		// combiner la requette entière
-		$query = $wherequery . $orderquery;
-		return $this->selectAll($query, $values);
+			// combiner la requette entière
+			$query = $wherequery . $orderquery;
+			return $this->selectAll($query, $values);
+		} catch (Exception $e) {
+			MsgRepository::newError("Erreur lors de la récupération des dashboards", "Les dashboards n'ont pas pu être récupérés.\n" . $e->getMessage());
+			return [];
+		} catch (PDOException $e) {
+			MsgRepository::newError("Erreur lors de la récupération des dashboards", "Les dashboards n'ont pas pu être récupérés.\n" . $e->getMessage());
+			return [];
+		}
 	}
-	
+
 	public function update_dashboard_by_id(Dashboard $dash, $componantsToDelete)
 	{
 		$this->update($dash, $dash->get_id());
@@ -105,35 +147,44 @@ class DashboardRepository extends AbstractRepository
 
 	public function save_new_dashboard(Dashboard $dash)
 	{
-		$values = $dash->formatTableau();
-		$values[":createur_id"] = UserManagement::getUser()->getId();
-		$values[":id"] = NULL;
-		$dashId = (int) $this->create($dash, $values);
+		try {
+			$values = $dash->formatTableau();
+			$values[":createur_id"] = UserManagement::getUser()->getId();
+			$values[":id"] = NULL;
+			$dashId = (int) $this->create($dash, $values);
 
-		// enregistrer les liens critereGeo
-		foreach ($dash->get_region() as $type => $ids) {
-			$DB_type = DashboardRepository::REVERSE_TYPE_GEO[$type];
-			foreach ($ids as $value) {
-				$query = "INSERT INTO CritereGeo_dashboard (dashboard_id, type_critere, critere_id) VALUES (:dashboard_id, :type_critere, :critere_id)";
-				$values = [":dashboard_id" => $dashId, ":type_critere" => $DB_type, ":critere_id" => $value];
+			// enregistrer les liens critereGeo
+			foreach ($dash->get_region() as $type => $ids) {
+				$DB_type = DashboardRepository::REVERSE_TYPE_GEO[$type];
+				foreach ($ids as $value) {
+					$query = "INSERT INTO CritereGeo_dashboard (dashboard_id, type_critere, critere_id) VALUES (:dashboard_id, :type_critere, :critere_id)";
+					$values = [":dashboard_id" => $dashId, ":type_critere" => $DB_type, ":critere_id" => $value];
+					DatabaseConnection::executeQuery($query, $values);
+				}
+			}
+
+			// parcourir les composants et les enregistrés
+			foreach ($dash->get_composants() as $comp) {
+				$compId = (new ComposantRepository)->save($comp);
+
+				// enregistrer les liens dashboard composants
+				$query = "INSERT INTO Composant_dashboard (dashboard_id, composant_id) VALUES (:dashboard_id, :composant_id);";
+				$values = [":composant_id" => $compId, ":dashboard_id" => $dashId];
 				DatabaseConnection::executeQuery($query, $values);
 			}
+
+			return $dashId;
+		} catch (Exception $e) {
+			MsgRepository::newError("Erreur lors de la sauvegarde du dashboard", "Le dashboard n'a pas pu être sauvegardé.\n" . $e->getMessage());
+			return null;
+		} catch (PDOException $e) {
+			MsgRepository::newError("Erreur lors de la sauvegarde du dashboard", "Le dashboard n'a pas pu être sauvegardé.\n" . $e->getMessage());
+			return null;
 		}
-
-		// parcourir les composants et les enregistrés
-		foreach ($dash->get_composants() as $comp) {
-			$compId = (new ComposantRepository)->save($comp);
-
-			// enregistrer les liens dashboard composants
-			$query = "INSERT INTO Composant_dashboard (dashboard_id, composant_id) VALUES (:dashboard_id, :composant_id);";
-			$values = [":composant_id" => $compId, ":dashboard_id" => $dashId];
-			DatabaseConnection::executeQuery($query, $values);
-		}
-
-		return $dashId;
 	}
 
-	public function delete_dashboard(Dashboard $dash): void {
+	public function delete_dashboard(Dashboard $dash): void
+	{
 		// supprimer le dashboard
 
 		// supprimer les liens géo
