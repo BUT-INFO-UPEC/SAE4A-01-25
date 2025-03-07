e7f2486a640759d292c80e78fca021e5f17decbf
Aminb <amine.bouras33@gmail.com>
2025-03-07 16:18:11 +0100

    tableau fonctionnel mais pas css

--- src/Views/Content/Dashboard/Visualisations/generate_tab_representation.php
+++ src/Views/Content/Dashboard/Visualisations/generate_tab_representation.php
@@ -1,25 +1,22 @@
 <div class='dashboard-card' id='comp<?= $params['chartId'] ?>'>
-	<h4><?= $params['titre'] ?></h4>
+    <h4><?= $params['titre'] ?></h4>
 
-	<!-- <p><?= htmlspecialchars($data) ?></p> -->
-	<!-- AJouter une mise en forme de $data en tableau -->
-	<table>
-    <thead>
-        <tr>
-            <?php foreach ($data as $header): ?>
-                <th><?= htmlspecialchars($header) ?></th>
-            <?php endforeach; ?>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach($data as $donnee): ?>
+    <!-- AJouter une mise en forme de $data en tableau -->
+    <table>
+        <thead>
             <tr>
-                <?php foreach ($donnee as $value): ?>
-                    <td><?= htmlspecialchars($value) ?></td>
+                <?php foreach ($data as $header): ?>
+                    <th><?= htmlspecialchars($header[0]) ?></th>
                 <?php endforeach; ?>
             </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            <tr>
+                <?php foreach ($data as $donnee): ?>
+                    <td><?= htmlspecialchars($donnee[1]) ?></td>
+                <?php endforeach; ?>
+            </tr>
+        </tbody>
+    </table>
 
 </div>
