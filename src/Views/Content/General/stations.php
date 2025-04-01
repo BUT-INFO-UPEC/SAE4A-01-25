0c7b590f760445c1f151948e3cc1a8202300501c
Kirito140 <maxence.campourcy@gmail.com>
2025-04-01 21:11:11 +0200

    tri auto de la colonne 'Nom Stations'

--- src/Views/Content/General/stations.php
+++ src/Views/Content/General/stations.php
@@ -1,42 +1,46 @@
-<div class="p-3">
-	<!-- Ajouter les fichiers CSS et JS pour DataTables -->
-	<link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-	<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+<!-- Ajouter les fichiers CSS et JS pour DataTables -->
+<link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
 
+<style>
+	tbody tr {
+		cursor: pointer;
+	}
+</style>
 
-	<!-- Table avec l'ID "stations_table" -->
-	<table id="stations_table" class="table table-striped table-hover">
-		<thead>
+<!-- Table avec l'ID "stations_table" -->
+<table id="stations_table" class="table table-striped table-hover">
+	<thead>
+		<tr>
+			<th>Nom Station</th>
+			<th>Ville</th>
+			<th>Départment</th>
+			<th>Région</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ($stations as $item) : ?>
 			<tr>
-				<th>Nom Station</th>
-				<th>Ville</th>
-				<th>Départment</th>
-				<th>Région</th>
+				<td><?= $item['station_name'] ?></td>
+				<td><?= $item['ville_name'] ?></td>
+				<td><?= $item['dept_name'] ?></td>
+				<td><?= $item['region_name'] ?></td>
 			</tr>
-		</thead>
-		<tbody>
-			<?php foreach ($stations as $item) : ?>
-				<tr>
-					<td><?= $item['station_name'] ?></td>
-					<td><?= $item['ville_name'] ?></td>
-					<td><?= $item['dept_name'] ?></td>
-					<td><?= $item['region_name'] ?></td>
-				</tr>
-			<?php endforeach; ?>
-		</tbody>
-	</table>
-</div>
+		<?php endforeach; ?>
+	</tbody>
+</table>
 
 <script>
 	// Activer DataTables sur le tableau
 	$(document).ready(function() {
 		$('#stations_table').DataTable({
 			// Options personnalisées si nécessaire
-			order: [], // Pas de tri par défaut
+			order: [
+				[0, 'asc'], // Tri par défaut sur la première colonne (Nom Station)
+			], // Pas de tri par défaut
 			searching: true, // Activer la recherche
 			paging: false, // Activer la pagination
-			info: false, // Désactiver l'info sur le nombre d'éléments
+			info: true, // Désactiver l'info sur le nombre d'éléments
 		});
 	});
 </script>
