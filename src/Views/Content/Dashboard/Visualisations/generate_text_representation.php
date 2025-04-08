1f7c6ad7b2b9164d0cea00cfd9eee78b63339117
Kirito140 <maxence.campourcy@gmail.com>
2025-04-08 14:40:00 +0200

    amelioration de la visu texte avec un padding sur la div du text  pour pas que ce soit collé au bords

--- src/Views/Content/Dashboard/Visualisations/generate_text_representation.php
+++ src/Views/Content/Dashboard/Visualisations/generate_text_representation.php
@@ -1,41 +1,26 @@
 <div class='dashboard-card' id='comp<?= $params['chartId'] ?>'>
-	<h4><?= htmlspecialchars($params['titre']) ?></h4>
+    <h4><?= htmlspecialchars($params['titre']) ?></h4>
 
-	<div class="d-flex justify-content-center align-items-center h-100">
-		<p id="textValue<?= $params['chartId'] ?>" class="composant_componenent"
-			style="font-size: 24px; font-weight: bold; text-align: center; transition: color 0.5s;">
-			Chargement...
-		</p>
-	</div>
+    <div class="d-flex justify-content-center align-items-center h-100 p-5">
+        <p id="textValue<?= $params['chartId'] ?>" class="composant_componenent"
+            style="font-size: 24px; font-weight: bold; text-align: center; transition: color 0.5s;">
+            Chargement...
+        </p>
+    </div>
 
-	<script>
-		document.addEventListener("DOMContentLoaded", function() {
-			var rawData = <?= json_encode($data) ?>;
-			console.log("Données reçues:", rawData);
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var rawData = <?= json_encode($data) ?>;
+            console.log("Données reçues:", rawData);
 
-			var totalValue = rawData["total"] !== undefined ? rawData["total"] : "Donnée indisponible";
-			var textElement = document.getElementById("textValue<?= $params['chartId'] ?>");
+            var totalValue = rawData["total"] !== undefined ? rawData["total"] : "Donnée indisponible";
+            var textElement = document.getElementById("textValue<?= $params['chartId'] ?>");
 
-            textElement.innerText = totalValue;
+            // Détermine la couleur en fonction de la valeur
+            var color = "grey";
+
+            textElement.innerText = isNaN(totalValue) ? totalValue : totalValue.toFixed(3);
             textElement.style.color = color;
-
-			// Détermine la couleur en fonction de la valeur
-			var color;
-			if (!isNaN(totalValue)) {
-				totalValue = parseFloat(totalValue);
-				if (totalValue < 100) {
-					color = "red"; // Faible valeur
-				} else if (totalValue < 500) {
-					color = "orange"; // Moyenne
-				} else {
-					color = "green"; // Valeur élevée
-				}
-			} else {
-				color = "black"; // Valeur non numérique
-			}
-
-			textElement.innerText = totalValue;
-			textElement.style.color = color;
-		});
-	</script>
+        });
+    </script>
 </div>
