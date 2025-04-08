1579c8e9e20b3bca51a31acb7c6825a11514f591
Aminb <amine.bouras33@gmail.com>
2025-04-08 14:29:51 +0200

    text en gris

--- src/Views/Content/Dashboard/Visualisations/generate_text_representation.php
+++ src/Views/Content/Dashboard/Visualisations/generate_text_representation.php
@@ -1,41 +1,31 @@
 <div class='dashboard-card' id='comp<?= $params['chartId'] ?>'>
-	<h4><?= htmlspecialchars($params['titre']) ?></h4>
-
-	<div class="d-flex justify-content-center align-items-center h-100">
-		<p id="textValue<?= $params['chartId'] ?>" class="composant_componenent"
-			style="font-size: 24px; font-weight: bold; text-align: center; transition: color 0.5s;">
-			Chargement...
-		</p>
-	</div>
-
-	<script>
-		document.addEventListener("DOMContentLoaded", function() {
-			var rawData = <?= json_encode($data) ?>;
-			console.log("Données reçues:", rawData);
-
-			var totalValue = rawData["total"] !== undefined ? rawData["total"] : "Donnée indisponible";
-			var textElement = document.getElementById("textValue<?= $params['chartId'] ?>");
-
+<h4><?= htmlspecialchars($params['titre']) ?></h4>
+ 
+    <div class="d-flex justify-content-center align-items-center h-100">
+<p id="textValue<?= $params['chartId'] ?>" class="composant_componenent"
+            style="font-size: 24px; font-weight: bold; text-align: center; transition: color 0.5s;">
+            Chargement...
+</p>
+</div>
+ 
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var rawData = <?= json_encode($data) ?>;
+            console.log("Données reçues:", rawData);
+ 
+            var totalValue = rawData["total"] !== undefined ? rawData["total"] : null;
+            var unit = rawData["unite"] !== undefined ? rawData["unite"] : "";
+ 
+            var textElement = document.getElementById("textValue<?= $params['chartId'] ?>");
+ 
+            if (totalValue !== null && !isNaN(totalValue)) {
+                totalValue = parseFloat(totalValue).toFixed(3) + " " + unit;
+            } else {
+                totalValue = "Donnée indisponible";
+            }
+ 
             textElement.innerText = totalValue;
-            textElement.style.color = color;
-
-			// Détermine la couleur en fonction de la valeur
-			var color;
-			if (!isNaN(totalValue)) {
-				totalValue = parseFloat(totalValue);
-				if (totalValue < 100) {
-					color = "red"; // Faible valeur
-				} else if (totalValue < 500) {
-					color = "orange"; // Moyenne
-				} else {
-					color = "green"; // Valeur élevée
-				}
-			} else {
-				color = "black"; // Valeur non numérique
-			}
-
-			textElement.innerText = totalValue;
-			textElement.style.color = color;
-		});
-	</script>
+            textElement.style.color = "#808080";
+        });
+</script>
 </div>
